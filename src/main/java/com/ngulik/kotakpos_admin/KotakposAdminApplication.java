19e13c980fc4f2f5abcbad265031ad34cf09eec0
syarifulanam <em.syariful.anam@gmail.com>
2026-03-09 15:39:57 +0700

    new project

---
+++ src/main/java/com/ngulik/kotakpos_admin/KotakposAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ngulik.kotakpos_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotakposAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotakposAdminApplication.class, args);
+	}
+
+}
