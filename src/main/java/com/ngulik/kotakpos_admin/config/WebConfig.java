547169951f8971a963443d7600b7f37d28ea6363
syarifulanam <em.syariful.anam@gmail.com>
2026-04-03 09:19:37 +0700

    revisi lagi

--- src/main/java/com/ngulik/kotakpos_admin/config/WebConfig.java
+++ src/main/java/com/ngulik/kotakpos_admin/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.ngulik.kotakpos_admin.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,8 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/uploads/**")
+                    .addResourceLocations("file:" + uploadDir + "/");
 
        registry.addResourceHandler("/**")
                .addResourceLocations("classpath:/static/")
--- src/test/java/com/ngulik/kotakpos_admin/util/ProductHelperTest.java
+++ src/test/java/com/ngulik/kotakpos_admin/util/ProductHelperTest.java
@@ -4,8 +4,6 @@ import com.ngulik.kotakpos_admin.entity.Category;
 import com.ngulik.kotakpos_admin.entity.Product;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ProductHelperTest {
 
     @Test
